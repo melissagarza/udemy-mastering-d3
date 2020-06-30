776fc63fa2a6b648d87012b4ba508187498d08bf
Melissa Garza <melissa.garza@utexas.edu>
2020-06-30 15:18:42 -0500

    Start next section

---
+++ 3.02/js/main.js
@@ -0,0 +1,29 @@
+/*
+*    main.js
+*    Mastering Data Visualization with D3.js
+*    2.8 - Activity: Your first visualization!
+*/
+
+(async () => {
+
+  const dataBuildings = await d3.json('data/buildings.json');
+
+  dataBuildings.forEach(d => { d.height = +d.height });
+  console.log(dataBuildings);
+
+  const svg = d3.select('#chart-area').append('svg')
+    .attr('width', 500)
+    .attr('height', 500);
+
+  const rectangles = svg.selectAll('rect')
+    .data(dataBuildings);
+
+  rectangles.enter('rect')
+    .append('rect')
+      .attr('x', (d, i) => (i * 50) + 25)
+      .attr('y', 0)
+      .attr('width', 25)
+      .attr('height', d => d.height)
+      .attr('fill', 'gray');
+
+})();
