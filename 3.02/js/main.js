34fdf73c42d26d3c20da8160a3d39369a69a639a
Melissa Garza <melissa.garza@utexas.edu>
2020-06-30 15:21:00 -0500

    Add a scale to the height value of the buildings data

--- 3.02/js/main.js
+++ 3.02/js/main.js
@@ -11,6 +11,10 @@
   dataBuildings.forEach(d => { d.height = +d.height });
   console.log(dataBuildings);
 
+  const y = d3.scaleLinear()
+    .domain([0, 828])
+    .range([0, 400]);
+
   const svg = d3.select('#chart-area').append('svg')
     .attr('width', 500)
     .attr('height', 500);
@@ -23,7 +27,7 @@
       .attr('x', (d, i) => (i * 50) + 25)
       .attr('y', 0)
       .attr('width', 25)
-      .attr('height', d => d.height)
+      .attr('height', d => y(d.height))
       .attr('fill', 'gray');
 
 })();
