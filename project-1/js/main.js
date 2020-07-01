9c8053d8fbde4ee1be47b772c976a5bac7045ed7
Melissa Garza <melissa.garza@utexas.edu>
2020-07-01 16:44:56 -0500

    Set some constant variables and pull in the revenues JSON data, converting some strings to integers

--- project-1/js/main.js
+++ project-1/js/main.js
@@ -4,3 +4,25 @@
 *    Project 1 - Star Break Coffee
 */
 
+(async () => {
+
+  const margin = {
+    top: 10,
+    right: 10,
+    bottom: 150,
+    left: 100,
+  };
+  const widthSvg = 800;
+  const heightSvg = 500;
+  const widthChart = widthSvg - margin.left - margin.right;
+  const heightChart = heightSvg - margin.top - margin.bottom;
+
+  const dataRevenues = await d3.json('data/revenues.json');
+
+  dataRevenues.forEach(d => {
+    d.revenue = +d.revenue;
+    d.profit = +d.profit;
+  });
+  console.log(dataRevenues);
+
+})();
