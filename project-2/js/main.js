d005a95b09663f779016d4801aa7e0807a229f44
Melissa Garza <melissa.garza@utexas.edu>
2020-07-08 16:07:00 -0500

    Set up the first year of the data to get the positioning and scales correct

--- project-2/js/main.js
+++ project-2/js/main.js
@@ -16,8 +16,15 @@
   };
   const widthChart = widthSvg - margin.left - margin.right;
   const heightChart = heightSvg - margin.top - margin.bottom;
+  const tr = d3.transition().duration(500);
+
+  let index = 0;
 
   const data = await d3.json('data/data.json');
+
+  data.forEach(d => {
+    d.year = +d.year;
+  });
 
   console.log(data);
 
@@ -28,6 +35,12 @@
   const scaleY = d3.scaleLinear()
     .domain([0, 100])
     .range([heightChart, 0]);
+
+  const scaleR = d3.scaleLinear()
+    .range([5, 25]);
+
+  const scaleColor = d3.scaleOrdinal()
+    .range(d3.schemePaired);
 
   const axisX = d3.axisBottom(scaleX)
     .tickValues([400, 4000, 40000])
@@ -46,17 +59,46 @@
 
   const groupAxisX = chart.append('g')
     .attr('transform', `translate(0, ${heightChart})`)
+    .transition(tr)
     .call(axisX);
 
   const groupAxisY = chart.append('g')
+    .transition(tr)
     .call(axisY);
 
   const update = (data) => {
+    const countriesByYear = data[index].countries.filter(c => c.life_exp !== null && c.income !== null);
 
+    console.log(countriesByYear);
+
+    scaleR.domain([
+      d3.min(countriesByYear, c => c.population),
+      d3.max(countriesByYear, c => c.population)
+    ]);
+
+    scaleColor.domain(countriesByYear.filter(c => c.country));
+
+    const points = chart.selectAll('circle')
+      .data(countriesByYear, d => d.country);
+
+    points.exit().remove();
+
+    points.enter()
+      .append('circle')
+        .attr('cx', d => scaleX(d.income))
+        .attr('cy', d => scaleY(d.life_exp))
+        .attr('r', d => scaleR(d.population))
+        .attr('fill', d => scaleColor(d.country));
   };
 
   d3.interval(() => {
-    update(data);
+
+    // update(data);
+
+    // index++;
+    // if (index === 214) index = 0;
   }, 1000);
+
+  update(data);
 
 })();
